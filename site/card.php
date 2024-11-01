7b93b4fb2569cda44903af62f0057a8575890679
CarolRN1611 <146491576+CarolRN1611@users.noreply.github.com>
2024-11-01 12:05:08 -0300

    #2
    modificando template
    
    Co-Authored-By: Deivid Victor <146492249+DeividSenai@users.noreply.github.com>

---
+++ site/card.php
@@ -0,0 +1,97 @@
+<div class="container-fluid py-4">
+      <div class="row">
+<div class="col-lg-6 col-12">
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-12">
+              <div class="card">
+                <span class="mask bg-primary opacity-10 border-radius-lg"></span>
+                <div class="card-body p-3 position-relative">
+                  <div class="row">
+                    <div class="col-8 text-start">
+                      <div class="icon icon-shape bg-white shadow text-center border-radius-2xl">
+                        <i class="ni ni-circle-08 text-dark text-gradient text-lg opacity-10" aria-hidden="true"></i>
+                      </div>
+                      <h5 class="text-white font-weight-bolder mb-0 mt-3">
+                        1600
+                      </h5>
+                      <span class="text-white text-sm">Clientes Ativos</span>
+                    </div>
+                    <div class="col-4">
+                      <div class="dropdown text-end mb-6">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-12 mt-4 mt-md-0">
+              <div class="card">
+                <span class="mask bg-dark opacity-10 border-radius-lg"></span>
+                <div class="card-body p-3 position-relative">
+                  <div class="row">
+                    <div class="col-8 text-start">
+                      <div class="icon icon-shape bg-white shadow text-center border-radius-2xl">
+                        <i class="ni ni-active-40 text-dark text-gradient text-lg opacity-10" aria-hidden="true"></i>
+                      </div>
+                      <h5 class="text-white font-weight-bolder mb-0 mt-3">
+                        357
+                      </h5>
+                      <span class="text-white text-sm">Processos Ativos</span>
+                    </div>
+                    <div class="col-4">
+                      <div class="dropstart text-end mb-6">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row mt-4">
+            <div class="col-lg-6 col-md-6 col-12">
+              <div class="card">
+                <span class="mask bg-dark opacity-10 border-radius-lg"></span>
+                <div class="card-body p-3 position-relative">
+                  <div class="row">
+                    <div class="col-8 text-start">
+                      <div class="icon icon-shape bg-white shadow text-center border-radius-2xl">
+                        <i class="ni ni-cart text-dark text-gradient text-lg opacity-10" aria-hidden="true"></i>
+                      </div>
+                      <h5 class="text-white font-weight-bolder mb-0 mt-3">
+                        2300
+                      </h5>
+                      <span class="text-white text-sm">Purchases</span>
+                    </div>
+                    <div class="col-4">
+                      <div class="dropdown text-end mb-6">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-12 mt-4 mt-md-0">
+              <div class="card">
+                <span class="mask bg-dark opacity-10 border-radius-lg"></span>
+                <div class="card-body p-3 position-relative">
+                  <div class="row">
+                    <div class="col-8 text-start">
+                      <div class="icon icon-shape bg-white shadow text-center border-radius-2xl">
+                        <i class="ni ni-like-2 text-dark text-gradient text-lg opacity-10" aria-hidden="true"></i>
+                      </div>
+                      <h5 class="text-white font-weight-bolder mb-0 mt-3">
+                        940
+                      </h5>
+                      <span class="text-white text-sm">Likes</span>
+                    </div>
+                    <div class="col-4">
+                      <div class="dropstart text-end mb-6">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
---
+++ site/dashboard.php
@@ -0,0 +1,13 @@
+
+<?php
+include "head.php";
+  include "menu.php";
+    include "navbar.php";
+    
+        include "card.php";
+        include "reviews.php";
+      include "footer.php";
+    
+  include "js.php" 
+
+?>
