5f9c90c4a6e1c84982881e5b0c8acd0ddf5e7153
CarolRN1611 <146491576+CarolRN1611@users.noreply.github.com>
2024-11-08 13:36:08 -0300

    #update

--- site/template/minhaAgenda.php
+++ site/template/minhaAgenda.php
@@ -20,119 +20,158 @@
     </style>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
-                    var calendarEl = document.getElementById('calendar');
+            var calendarEl = document.getElementById('calendar');
 
-                    var calendar = new FullCalendar.Calendar(calendarEl, {
-                        headerToolbar: {
-                            left: 'prev,next today',
-                            center: 'title',
-                            right: 'dayGridMonth,timeGridWeek,timeGridDay'
-                        },
-                        initialDate: '2024-11-07',
-                        navLinks: true,
-                        selectable: true,
-                        selectMirror: true,
-                        locale: 'pt-br',
-                        editable: true,
-                        dayMaxEvents: true,
-                        events: '../site/scripts/agenda.php',
-                        eventClick: function(info) {
-                            
-                            document.getElementById('visualizar_titulo').innerHTML = info.event.title;
-                            document.getElementById('visualizar_inicio').innerHTML = info.event.start.toLocaleString();
-                            document.getElementById('visualizar_fim').innerHTML = info.event.end ? info.event.end.toLocaleString() : 'Data não disponível';
-                            document.getElementById('visualizar_descricao').innerHTML = info.event.extendedProps.descricao;
-                             document.getElementById('visualizar_cliente').innerHTML = info.event.extendedProps.cliente;
-                            const usuario = info.event.extendedProps.id_usuario || 'Não disponível';
-                            document.getElementById('visualizar_usuario').innerHTML = usuario;
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                headerToolbar: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                },
+                initialDate: '2024-11-07',
+                navLinks: true,
+                selectable: true,
+                selectMirror: true,
+                locale: 'pt-br',
+                editable: true,
+                dayMaxEvents: true,
+                events: '../site/scripts/agenda.php',
+                eventClick: function(info) {
 
-                            document.getElementById('excluirEvento').dataset.eventId = info.event.id;
+                    document.getElementById('visualizar_titulo').innerHTML = info.event.title;
+                    document.getElementById('visualizar_inicio').innerHTML = info.event.start.toLocaleString();
+                    document.getElementById('visualizar_fim').innerHTML = info.event.end ? info.event.end.toLocaleString() : 'Data não disponível';
+                    document.getElementById('visualizar_descricao').innerHTML = info.event.extendedProps.descricao;
+                    document.getElementById('visualizar_cliente').innerHTML = info.event.extendedProps.cliente;
+                    const usuario = info.event.extendedProps.id_usuario || 'Não disponível';
+                    document.getElementById('visualizar_usuario').innerHTML = usuario;
+                    document.getElementById('visualizar_status').innerHTML = info.event.extendedProps.status;
+                    document.getElementById('excluirEvento').dataset.eventId = info.event.id;
+                    eventoIdGlobal = info.event.id;
 
-                            
-                            new bootstrap.Modal(document.getElementById('visualizarModal')).show();
-
-                            
-                            
-                        }
+                    new bootstrap.Modal(document.getElementById('visualizarModal')).show();
 
 
-                    });
 
-                        calendar.render();
-                        const excluirButton = document.getElementById('excluirEvento');
-                        if (excluirButton) {
-                            excluirButton.addEventListener('click', function() {
-                                const eventId = this.dataset.eventId;
-                                if (confirm("Tem certeza que deseja excluir este evento?")) {
+                }
 
-                                    fetch('../site/scripts/del_agenda.php', {
-                                            method: 'POST',
-                                            headers: {
-                                                'Content-Type': 'application/x-www-form-urlencoded'
-                                            },
-                                            body: `id=${eventId}`
-                                        })
-                                        .then(response => response.text())
-                                        .then(data => {
-                                            alert(data);
-                                            calendar.refetchEvents();
-                                            new bootstrap.Modal(document.getElementById('visualizarModal')).hide(); // 
-                                        })
-                                        .catch(error => {
-                                            alert('Erro ao tentar excluir o evento');
-                                        });
-                                }
+
+            });
+
+            calendar.render();
+            const excluirButton = document.getElementById('excluirEvento');
+            if (excluirButton) {
+                excluirButton.addEventListener('click', function() {
+                    const eventId = this.dataset.eventId;
+                    if (confirm("Tem certeza que deseja excluir este evento?")) {
+
+                        fetch('../site/scripts/del_agenda.php', {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/x-www-form-urlencoded'
+                                },
+                                body: `id=${eventId}`
+                            })
+                            .then(response => response.text())
+                            .then(data => {
+                                alert(data);
+                                calendar.refetchEvents();
+                                new bootstrap.Modal(document.getElementById('visualizarModal')).hide();
+                            })
+                            .catch(error => {
+                                alert('Erro ao tentar excluir o evento');
                             });
-                        }
+                    }
+                });
+            }
 
-                    });
+        });
     </script>
 </head>
 
 <body>
     <div id='calendar'></div>
 
-    <!-- Modal -->
+
     <div class="modal fade" id="visualizarModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="VisualizarModalLabel">Detalhes do Evento</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <dl class="row">
-                        <dt class="col-sm-3">Titulo:</dt>
-                        <dd class="col-sm-9" id="visualizar_titulo"></dd>
-                    </dl>
-                    <dl class="row">
-                        <dt class="col-sm-3">Inicio:</dt>
-                        <dd class="col-sm-9" id="visualizar_inicio"></dd>
-                    </dl>
-                    <dl class="row">
-                        <dt class="col-sm-3">Fim:</dt>
-                        <dd class="col-sm-9" id="visualizar_fim"></dd>
-                    </dl>
-                    <dl class="row">
-                        <dt class="col-sm-3">Descrição:</dt>
-                        <dd class="col-sm-9" id="visualizar_descricao"></dd>
-                    </dl>
-                    <dl class="row">
-                        <dt class="col-sm-3">Cliente:</dt>
-                        <dd class="col-sm-9" id="visualizar_cliente"></dd>
-                    </dl>
-                    <dl class="row">
-                        <dt class="col-sm-3">Responsável:</dt>
-                        <dd class="col-sm-9" id="visualizar_usuario"></dd>
-                    </dl>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" id="excluirEvento">Excluir</button>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                </div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="VisualizarModalLabel">Detalhes do Evento</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <dl class="row">
+                    <dt class="col-sm-3">Titulo:</dt>
+                    <dd class="col-sm-9" id="visualizar_titulo"></dd>
+                </dl>
+                <dl class="row">
+                    <dt class="col-sm-3">Inicio:</dt>
+                    <dd class="col-sm-9" id="visualizar_inicio"></dd>
+                </dl>
+                <dl class="row">
+                    <dt class="col-sm-3">Fim:</dt>
+                    <dd class="col-sm-9" id="visualizar_fim"></dd>
+                </dl>
+                <dl class="row">
+                    <dt class="col-sm-3">Descrição:</dt>
+                    <dd class="col-sm-9" id="visualizar_descricao"></dd>
+                </dl>
+                <dl class="row">
+                    <dt class="col-sm-3">Cliente:</dt>
+                    <dd class="col-sm-9" id="visualizar_cliente"></dd>
+                </dl>
+                <dl class="row">
+                    <dt class="col-sm-3">Responsável:</dt>
+                    <dd class="col-sm-9" id="visualizar_usuario"></dd>
+                </dl>
+                <!-- Campo hidden para armazenar o ID do evento -->
+                <input type="hidden" id="visualizar_evento_id" value="1"> <!-- Exemplo: O valor do evento seria atribuído dinamicamente -->
+            </div>
+
+            <div class="form-check d-flex justify-content-end me-3 mt-3">
+                <input class="form-check-input" type="checkbox" id="visualizar_status" onchange="atualizarStatus(this)">
+                <label class="form-check-label ms-2" for="visualizar_status">
+                    Marcar como concluída
+                </label>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" id="excluirEvento">Excluir</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
             </div>
         </div>
     </div>
+</div>
+
+
+    <script>
+        
+        function atualizarStatus(checkbox) {
+            
+            const eventoId = document.getElementById('visualizar_evento_id').value; 
+            const status = checkbox.checked ? 1 : 0;
+
+            console.log(eventoId)
+            console.log(status)
+
+            const formData = new FormData();
+            formData.append('evento_id', eventoId); 
+            formData.append('status', status); 
+
+            fetch('../site/scripts/status_evento.php', {
+                    method: 'POST',
+                    body: formData
+                })
+                .then(response => response.text()) // ou .json() dependendo da resposta do servidor
+                .then(data => {
+                    console.log(data); // Aqui você pode exibir uma mensagem ou fazer algo quando a requisição for bem-sucedida
+                })
+                .catch(error => {
+                    console.error('Erro ao atualizar status:', error);
+                });
+        }
+    </script>
     <script src='../site/js/index.global.js'></script>
     <script src='../../SUITS/assets/js/core/pt-br.global.js'></script>
 </body>
