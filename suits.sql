6d7f7829b49eb65cf0f14f8a0b789ad50d63a95d
Caroline Rocha <146491576+CarolRN1611@users.noreply.github.com>
2024-11-11 18:54:26 -0300

    Update suits.sql
    correções

--- suits.sql
+++ suits.sql
@@ -1,34 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 05/11/2024 às 01:08
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `suits`
---
 
 CREATE DATABASE suits;
 USE suits;
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `funcionarios`
---
 
 CREATE TABLE clientes (  
     id INT AUTO_INCREMENT PRIMARY KEY,  
@@ -49,47 +22,6 @@ CREATE TABLE clientes (
     status VARCHAR(10) NOT NULL  
 );
 
-select *
-from clientes;
-
-select * from tarefas;
-
-CREATE TABLE tarefas (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(255) NOT NULL,
-    dataInicio DATE NOT NULL,
-    horarioInicio TIME NOT NULL,
-    dataFinal DATE NOT NULL,
-    horarioFinal TIME NOT NULL,
-    cliente VARCHAR(255) NOT NULL,
-    descricao TEXT,
-    cor VARCHAR(7),
-    status TINYINT(1) DEFAULT 0,  
-    id_usuario INT,
-    FOREIGN KEY (id_usuario) REFERENCES funcionarios(id)
-) CHARSET=utf8mb4;
-
-
-INSERT INTO tarefa (titulo, dataInicio, horarioInicio, dataFinal, horarioFinal, cliente, descricao, cor, id_usuario)
-VALUES 
-('Reunião com cliente X', '2024-11-10', '10:00:00', '2024-11-10', '12:00:00', 'Cliente X', 'Reunião para discutir o projeto', '#FF5733', 1);
-INSERT INTO tarefa (titulo, dataInicio, horarioInicio, dataFinal, horarioFinal, cliente, descricao, cor, id_usuario)
-VALUES 
-('Reunião de equipe', '2024-11-11', '14:00:00', '2024-11-11', '16:00:00', 'Equipe interna', 'Reunião semanal de alinhamento de tarefas', '#33FF57', 2);
-
-
-
-
-CREATE TABLE processos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    numero_processo VARCHAR(100) NOT NULL,
-    data DATE NOT NULL,
-    horario TIME NOT NULL,
-    vara VARCHAR(200) NOT NULL,
-    cliente VARCHAR(200) NOT NULL
-);
-
-
 CREATE TABLE `funcionarios` (
   `id` int(11) PRIMARY KEY AUTO_INCREMENT,
   `nome` varchar(50) NOT NULL,
@@ -109,27 +41,40 @@ CREATE TABLE `funcionarios` (
   `status` enum('Ativo','Inativo') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Índices para tabelas despejadas
---
 
---
--- Índices de tabela `funcionarios`
---
-ALTER TABLE `funcionarios`
-  ADD PRIMARY KEY (`id`);
+CREATE TABLE tarefas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    dataInicio DATE NOT NULL,
+    horarioInicio TIME NOT NULL,
+    dataFinal DATE NOT NULL,
+    horarioFinal TIME NOT NULL,
+    cliente VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    cor VARCHAR(7),
+    status TINYINT(1) DEFAULT 0,  
+    id_usuario INT,
+    FOREIGN KEY (id_usuario) REFERENCES funcionarios(id)
+) CHARSET=utf8mb4;
 
---
--- AUTO_INCREMENT para tabelas despejadas
---
 
---
--- AUTO_INCREMENT de tabela `funcionarios`
---
-ALTER TABLE `funcionarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE processos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    numero_processo VARCHAR(100) NOT NULL,
+    data DATE NOT NULL,
+    horario TIME NOT NULL,
+    vara VARCHAR(200) NOT NULL,
+    cliente VARCHAR(200) NOT NULL
+);
+
+
+
+
+select *
+from clientes;
+
+select * from tarefas;
+
+
+
